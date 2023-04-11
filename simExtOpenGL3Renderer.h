eed072e5d35fbdb6c2d63f2bc1f84d555d284fda
Coppelia <mfreese@gmx.ch>
2023-04-11 16:53:35 +0200

    small refact + fvisibility=hidden

--- simExtOpenGL3Renderer.h
+++ simExtOpenGL3Renderer.h
@@ -2,13 +2,7 @@
 #define SIMEXTEXTERNALRENDERER_H
 
 #include <QtCore/qglobal.h>
-
-#ifdef _WIN32
-    #define SIM_DLLEXPORT extern "C" __declspec(dllexport)
-#endif /* _WIN32 */
-#if defined (__linux) || defined (__APPLE__)
-    #define SIM_DLLEXPORT extern "C"
-#endif /* __linux || __APPLE__ */
+#include <simLib/simExp.h>
 
 // The 3 required entry points of the plugin:
 SIM_DLLEXPORT unsigned char simStart(void* reservedPointer,int reservedInt);
